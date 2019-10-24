add63e228ecd86c70a963dac270f7ac9571e4aac
Kirk.Wang <kirk.w.wang@gmail.com>
2019-10-24 22:59:24 +0800

    update hello-blocks

--- demo/hello-blocks/server/main.go
+++ demo/hello-blocks/server/main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/lonng/nano"
+	"github.com/lonng/nano/component"
+	"github.com/lonng/nano/serialize/json"
+	"github.com/lonng/nano/session"
 )
 
-type room struct {
-	group *nano.Group
+type Room struct {
+	Group *nano.Group
+}
+
+type RoomManager struct {
+	component.Base
+	Rooms map[int]*Room
+}
+
+type JoinResponse struct {
+	Code   int    `json:"code"`
+	Result string `json:"result"`
+}
+
+func (r *RoomManager) Init() {
+	fmt.Println("RoomManager~Init")
+}
+
+func (r *RoomManager) Join(s *session.Session, msg []byte) error {
+	return s.Response(&JoinResponse{Code: 1, Result: "Join~~Sucessed~~"})
 }
 
 func init() {
@@ -15,10 +37,20 @@ func init() {
 }
 
 func main() {
+
+	components := &component.Components{}
+	components.Register(
+		&RoomManager{},
+		component.WithName("room"),
+		component.WithNameFunc(strings.ToLower),
+	)
+
 	nano.Listen(
 		":3575",
 		nano.WithDebugMode(),
 		nano.WithIsWebsocket(true),
 		nano.WithWSPath("/game"),
+		nano.WithComponents(components),
+		nano.WithSerializer(json.NewSerializer()),
 	)
 }
